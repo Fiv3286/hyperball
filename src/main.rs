d825805796b9c7a7d53d401bb1371f924ea33d3b
Egor Maximovich Krasnoperov <e.m.krasnoperov@student.utwente.nl>
2024-01-25 17:45:43 +0100

    generating blank plots

--- src/main.rs
+++ src/main.rs
@@ -10,6 +10,8 @@ use petgraph_gen::random_gnm_graph;
 
 
 use petgraph::data::FromElements;
+use plotters::backend::BitMapBackend;
+use plotters::drawing::IntoDrawingArea;
 use rand::rngs::StdRng;
 use rand::RngCore;
 use std::collections::hash_map::RandomState;
@@ -32,7 +34,7 @@ fn main() {
     let precision:u8 = 8;
     let mut rng = rand::thread_rng();
 
-    let ratios: Vec<f64> = (0..=100).map(|x| f64::from(x) *0.1).collect();
+    let ratios: Vec<f64> = (0..=100).map(|x| f64::from(x) *0.1).collect(); // 0 to 100, / 10 to generate floats for ratio.
     
     for ratio in ratios.iter() {
         println!("{:?}", ratio);
@@ -54,7 +56,27 @@ fn main() {
                 let g: UnGraph<(), ()> = random_gnm_graph(&mut rng, n, m);
     
                 // let (time, _) = mst_time(g);
-                let logbook = hyperball(g, precision);
+                let logbook: Vec<(u8, Vec<HyperLogLogPlus<u8, RandomState>>)> = hyperball(g, precision);
+
+                let path = &format!("images/{}x{}p{}.png", v, e, ratio);
+                let root = BitMapBackend::new(path, (640, 480)).into_drawing_area();
+
+                for (t, mut entry) in logbook.clone() {
+
+                    let new_harmonic_mean: (f64, i32) = entry.iter_mut().fold((0.0, 0), |acc: (f64, i32), element: &mut HyperLogLogPlus<u8, RandomState>| {
+                        (acc.0 + 1.0 / element.count(), acc.1 + 1)
+                    });
+                    
+                    let harmonic_mean: f64 = if new_harmonic_mean.1 == 0 {
+                        0.0 // Handle division by zero if the iterator is empty
+                    } else {
+                        new_harmonic_mean.1 as f64 / new_harmonic_mean.0
+                    };
+        
+
+                    println!("dimensions {:?}, {:?}, {:?}. t {:?}, mean {:?}", n, m, ratio, t, harmonic_mean)
+                }
+
                 let (_t, hllp_counters) = logbook.last().unwrap();
                     
                 let mut merged_counter = hllp_counters
